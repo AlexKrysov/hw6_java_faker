9ed560b364521e5bc12c2eb1dae109e216d17bb3
Alex Krysov <krysov-95@mail.ru>
2022-11-25 13:41:09 +0600

    added 3

--- src/test/java/com/krysov/tests/RegistrationWithFaker.java
+++ src/test/java/com/krysov/tests/RegistrationWithFaker.java
@@ -1,31 +1,17 @@
 package com.krysov.tests;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
+import static com.krysov.tests.TestData.*;
 
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-
-public class RegistrationWithPageObjectsTests extends com.krysov.tests.TestBase {
-
+public class RegistrationWithFaker extends com.krysov.tests.TestBase {
     @Test
     void successfulRegistrationTest() {
-        String userName = "Alex";
-        String lastName = "Ivanov";
-        String Email = "Ivan@ivanov.com";
-        String gender = "Other";
-        String Mobile = "1231231234";
-        String dayBirth = "30";
-        String monthBirth = "July";
-        String yearBirth = "2008";
-        String Subjects = "biology";
-        String Hobbies = "Sports";
-        String Picture = "img/1.png";
-        String CurrentAddress = "street 7";
-        String State = "NCR";
-        String City = "Delhi";
+
+
 
         registrationPage.openPage()
-                .setFirstName(userName)
+                .setFirstName(name)
                 .setLastName(lastName)
                 .setEmail(Email)
                 .setGender(gender)
@@ -39,12 +25,13 @@ public class RegistrationWithPageObjectsTests extends com.krysov.tests.TestBase 
                 .setCity(City)
                 .submit()
                 .verifyResultsModalAppears()
-                .verifyResult("Student Name", userName + " " + lastName)
+                .verifyResult("Student Name", name + " " + lastName)
                 .verifyResult("Student Email", Email)
                 .verifyResult("Gender", gender)
                 .verifyResult("Mobile", Mobile)
                 .verifyResult("Date of Birth", dayBirth+" "+ monthBirth+","+yearBirth)
                 .verifyResult("State and City", State+ " "+City)
                 .verifyResult("Subjects", Subjects);
+
     }
 }
---
+++ src/test/java/com/krysov/tests/TestData.java
@@ -0,0 +1,29 @@
+package com.krysov.tests;
+
+import com.github.javafaker.Faker;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class TestData {
+
+    public static Faker faker = new Faker();
+    public static String name = faker.name().firstName();
+    public static String lastName = faker.name().lastName();
+    public static String Email = faker.internet().emailAddress();
+    public static String CurrentAddress = faker.address().fullAddress();
+    public static String Mobile = faker.phoneNumber().subscriberNumber(10);
+    public static SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy", Locale.ENGLISH);
+    public static String[] date = dateFormat.format(faker.date().birthday()).split(" ");
+    public static String dayBirth = date[0];
+    public static String monthBirth = date[1];
+    public static String yearBirth = date[2];
+    public static
+    String
+            gender = "Other",
+            Subjects = "biology",
+            Hobbies = "Sports",
+            Picture = "img/1.png",
+            State = "NCR",
+            City = "Delhi";
+}
